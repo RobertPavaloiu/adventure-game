33f858f10f283ecd8c2fce721c22979de82454c0
RobertPavaloiu <robert.pavaloiu@ziffdavis.com>
2023-07-25 19:50:01 +0100

    added damage mechanics

--- main.js
+++ main.js
@@ -1,66 +1,102 @@
 // Player Classes
-class Player{
-constructor(maxHealth, currentHealth, damage, critical, potions){
+class Player {
+  constructor(maxHealth, currentHealth, damage, critical, potions) {
     this.maxHealth = maxHealth;
     this.currentHealth = currentHealth;
     this.damage = damage;
     this.critical = critical;
     this.potions;
     this.criticalChance = 10;
-}
+  }
+  takeDamage(amount) {
+    this.currentHealth -= amount;
+  }
+  usePotion() {
+    if (this.currentHealth + 20 >= this.maxHealth) {
+      this.currentHealth = this.maxHealth;
+    } else {
+      this.currentHealth += 20;
+    }
+  }
+  dealDamage() {
+    let chance = randomChance(1, this.criticalChance);
+    if (chance === this.critical) {
+      return this.critical;
+    } else {
+      return this.damage;
+    }
+  }
 }
 
-class Wizard extends Player{
-    constructor(currentHealth){
-        this.currentHealth = currentHealth;
-        super(criticalChance)
-        this.damage = 8;
-        this.critical = 16;
-        this.potions = 4;
-        this.maxHealth = 80;
-    }
+class Wizard extends Player {
+  constructor() {
+    this.maxHealth = 80;
+    this.currentHealth = this.maxHealth;
+    super(criticalChance);
+    this.damage = 8;
+    this.critical = 16;
+    this.potions = 4;
+  }
 }
 
-class Barbarian extends Player{
-    constructor(currentHealth){
-        this.currentHealth = currentHealth;
-        super(criticalChance)
-        this.damage = 4;
-        this.critical = 10;
-        this.potions = 2;
-        this.maxHealth = 160;
-    }
+class Barbarian extends Player {
+  constructor() {
+    this.maxHealth = 160;
+    this.currentHealth = this.maxHealth;
+    super(criticalChance);
+    this.damage = 4;
+    this.critical = 10;
+    this.potions = 2;
+  }
 }
 
-class Thief extends Player{
-    constructor(currentHealth){
-        this.currentHealth = currentHealth;
-        super(criticalChance)
-        this.damage = 6;
-        this.critical = 12;
-        this.potions = 3;
-        this.maxHealth = 120;
-    }
+class Thief extends Player {
+  constructor() {
+    this.maxHealth = 120;
+    this.currentHealth = this.maxHealth;
+    super(criticalChance);
+    this.damage = 6;
+    this.critical = 12;
+    this.potions = 3;
+  }
 }
 
 //Enemy Classes
 
-class Enemy{
-    constructor(maxHealth, currentHealth, damage, critical){
-        this.maxHealth = maxHealth;
-        this.currentHealth = currentHealth;
-        this.damage = damage;
-        this.critical = critical;
-        this.criticalChance = 15;
-    }
+class Enemy {
+  constructor(maxHealth, currentHealth, damage, critical) {
+    this.maxHealth = maxHealth;
+    this.currentHealth = currentHealth;
+    this.damage = damage;
+    this.critical = critical;
+    this.criticalChance = 15;
+  }
+  takeDamage(amount) {
+    this.currentHealth -= amount;
+  }
 }
 
-class Slime extends Enemy{
-    constructor(currentHealth){
-        this.currentHealth = currentHealth;
-        super(criticalChance)
-        this.damage = 5;
-        this.critical = 10;
-        this.maxHealth = 40;
+class Slime extends Enemy {
+  constructor() {
+    this.maxHealth = 40;
+    this.currentHealth = this.maxHealth;
+    super(criticalChance);
+    this.damage = 5;
+    this.critical = 10;
+  }
+  dealDamage() {
+    let chance = randomChance(1, this.criticalChance);
+    if (chance === this.critical) {
+      return this.critical;
+    } else {
+      return this.damage;
     }
+  }
+}
+
+// Helper Functions
+
+function randomChance(min, max) {
+  // Generate a random integer between min and max.
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
